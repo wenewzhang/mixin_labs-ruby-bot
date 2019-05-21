8d6e5ed648f85d2fe46a244e0ee2b1e0cd4e378a
wenewzhang <wenewboy@gmail.com>
2019-05-21 10:21:00 +0800

    write wallet info to csv

--- bitcoin_wallet-ruby.rb
+++ bitcoin_wallet-ruby.rb
@@ -1,6 +1,7 @@
 require 'openssl'
 require '../mixin_bot/lib/mixin_bot'
 require 'yaml'
+require 'csv'
 
 yaml_hash = YAML.load_file('./config.yml')
 
@@ -14,7 +15,27 @@ access_token = MixinBot.api.access_token("GET","/","")
 
 rsa_key = OpenSSL::PKey::RSA.new(1024)
 private_key = rsa_key.to_pem()
+p private_key
 public_key = rsa_key.public_key.to_pem
 secret_client = public_key.sub("-----BEGIN PUBLIC KEY-----\n","").sub("\n-----END PUBLIC KEY-----\n","")
 reqInfo = MixinBot.api.create_user("ruby bot",secret_client)
-p reqInfo
+p reqInfo["data"]["pin_token"]
+p reqInfo["data"]["user_id"]
+p reqInfo["data"]["session_id"]
+
+
+CSV.open("mybitcoin_wallet.csv", "w") do |csv|
+  csv << [private_key, reqInfo["data"]["pin_token"], reqInfo["data"]["session_id"], reqInfo["data"]["user_id"]]
+end
+
+botAssetsInfo = MixinBot.api.read_assets()
+p botAssetsInfo
+
+MixinBot.client_id  = reqInfo["data"]["user_id"]
+MixinBot.session_id = reqInfo["data"]["session_id"]
+MixinBot.client_secret = ""
+MixinBot.pin_token   = reqInfo["data"]["pin_token"]
+MixinBot.private_key = private_key
+p "--------------------------------"
+botAssetsInfo2 = MixinBot.api.read_assets()
+p botAssetsInfo2
